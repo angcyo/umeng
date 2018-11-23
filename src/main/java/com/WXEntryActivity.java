01874235830c3536f818ce651ac2ea1347419bc6
angcyo <angcyo@126.com>
2018-11-23 10:15:15 +0800

    um 6.9.4

--- src/main/java/com/WXEntryActivity.java
+++ src/main/java/com/WXEntryActivity.java
@@ -7,14 +7,16 @@ package com;
  * 在包名目录下创建wxapi文件夹，
  * 新建一个名为WXEntryActivity的activity继承WXCallbackActivity。
  * 这里注意一定是包名路径下，例如我的包名是com.umeng.soexample,则配置如下：
- * (需要注意，如果使用精简版WXCallbackActivity的路径为com.umeng.weixin.callback.WXCallbackActivity,
- * 如果使用完整版路径是com.umeng.socialize.weixin.view.WXCallbackActivity)
+ *
+ * 路径是com.umeng.socialize.weixin.view.WXCallbackActivity)
  */
 //import com.umeng.weixin.callback.WXCallbackActivity;//精简版导入这个
 
 //完整版导入这个
 
 import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+//com.umeng.socialize.weixin.view.WXCallbackActivity
 
 /**
  * 请将此文件复制到 开发app的包名.wxapi 文件夹中.
---
+++ src/main/java/com/angcyo/umeng/UM.java
@@ -0,0 +1,191 @@
+package com.angcyo.umeng;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import com.umeng.socialize.Config;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.UmengTool;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.media.UMWeb;
+
+/**
+ * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
+ * 项目名称：
+ * 类的描述：
+ * 创建人员：Robi
+ * 创建时间：2017/05/18 10:00
+ * 修改人员：Robi
+ * 修改时间：2017/05/18 10:00
+ * 修改备注：
+ * Version: 1.0.0
+ */
+public class UM {
+
+    private static Application sApplication;
+
+    /**
+     * 请记得调用初始化方法
+     */
+    public static void init(Application application, boolean debug) {
+        sApplication = application;
+
+        Config.DEBUG = debug;
+
+        UMShareAPI.get(sApplication);
+
+        initPlatformConfig();
+    }
+
+    static void initPlatformConfig() {
+        try {
+            ApplicationInfo applicationInfo = sApplication.getPackageManager()
+                    .getApplicationInfo(sApplication.getPackageName(), PackageManager.GET_META_DATA);
+
+            String qq_id = String.valueOf(applicationInfo.metaData.get("QQ_ID"));
+            String qq_key = String.valueOf(applicationInfo.metaData.get("QQ_KEY"));
+            String wx_id = String.valueOf(applicationInfo.metaData.get("WX_ID"));
+            String wx_key = String.valueOf(applicationInfo.metaData.get("WX_KEY"));
+
+            PlatformConfig.setWeixin(wx_id, wx_key);
+            PlatformConfig.setQQZone(qq_id, qq_key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 删除授权
+     */
+    public static void deleteAuth(Activity activity, SHARE_MEDIA platform, UMAuthListener listener) {
+        UMShareAPI.get(sApplication).deleteOauth(activity, platform, listener);
+    }
+
+    /**
+     * 授权验证
+     */
+    public static void authVerify(Activity activity, SHARE_MEDIA platform, UMAuthListener listener) {
+        UMShareAPI.get(sApplication).doOauthVerify(activity, platform, listener);
+    }
+
+    /**
+     * 返回是否授权
+     */
+    public static boolean isAuthorize(Activity activity, SHARE_MEDIA platform) {
+        return UMShareAPI.get(sApplication).isAuthorize(activity, platform);
+    }
+
+    /**
+     * 获取平台信息
+     * <p/>
+     * 如果未授权, 会拉取授权界面, 再返回数据
+     * <p/>
+     * 如果已授权, 会直接返回数据
+     */
+    public static void getPlatformInfo(Activity activity, SHARE_MEDIA platform, UMAuthListener listener) {
+        UMShareAPI.get(sApplication).getPlatformInfo(activity, platform, listener);
+    }
+
+    /**
+     * 分享纯文本
+     * <p/>
+     * <b>注意:QQ 不能分享纯文本</b>
+     */
+    public static void shareText(Activity activity, SHARE_MEDIA shareMedia,
+                                 String shareText, UMShareListener listener) {
+        new ShareAction(activity)
+                .setPlatform(shareMedia)
+                .withText(shareText)
+                .setCallback(listener)
+                .share();
+    }
+
+    /**
+     * 分享图片
+     * <p/
+     * 分享到微信必须要设置缩略图
+     */
+    public static void shareImage(Activity activity, SHARE_MEDIA shareMedia,
+                                  String imageUrl, int thumbRes,
+                                  UMShareListener listener) {
+        UMImage umImage = new UMImage(activity, imageUrl);
+        if (thumbRes != -1) {
+            UMImage umThumb = new UMImage(activity, thumbRes);
+            umImage.setThumb(umThumb);
+        }
+        new ShareAction(activity)
+                .setPlatform(shareMedia)
+                .withMedia(umImage)
+                .setCallback(listener)
+                .share();
+    }
+
+    /**
+     * 分享图文
+     */
+    public static void shareImageText(Activity activity, SHARE_MEDIA shareMedia,
+                                      String imageUrl, int thumbRes,
+                                      String text,
+                                      UMShareListener listener) {
+        UMImage umImage = new UMImage(activity, imageUrl);
+        if (thumbRes != -1) {
+            UMImage umThumb = new UMImage(activity, thumbRes);
+            umImage.setThumb(umThumb);
+        }
+        new ShareAction(activity)
+                .setPlatform(shareMedia)
+                .withMedia(umImage)
+                .withText(text)
+                .setCallback(listener)
+                .share();
+    }
+
+    /**
+     * 分享链接
+     */
+    public static void shareWeb(Activity activity, SHARE_MEDIA shareMedia,
+                                String url, int thumbRes,
+                                String title, String des,
+                                String text,
+                                UMShareListener listener) {
+        UMWeb web = new UMWeb(url);
+        web.setTitle(title);//标题
+        //缩略图
+        if (thumbRes != -1) {
+            UMImage umThumb = new UMImage(activity, thumbRes);
+            web.setThumb(umThumb);
+        }
+        web.setDescription(des);//描述
+
+        new ShareAction(activity)
+                .setPlatform(shareMedia)
+                .withMedia(web)
+                .withText(text)
+                .setCallback(listener)
+                .share();
+    }
+
+    public static void checkQQ(Activity activity) {
+        UmengTool.checkQQ(activity);
+    }
+
+    public static void checkWX(Activity activity) {
+        UmengTool.checkWx(activity);
+    }
+
+    public static void onActivityResult(int requestCode, int resultCode, Intent data) {
+        UMShareAPI.get(sApplication).onActivityResult(requestCode, resultCode, data);
+    }
+
+    public static void onDestroy() {
+        UMShareAPI.get(sApplication).release();
+    }
+}
